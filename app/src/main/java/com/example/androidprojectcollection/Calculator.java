900ea1605aaa4ad817fcd217f8b4d225e1cbbe36
LAB11_USER ON WS15 <L11X07W15@citulab.net>
2024-03-11 10:28:08 +0800

    NOT WORKING

---
+++ app/src/main/java/com/example/androidprojectcollection/Calculator.java
@@ -0,0 +1,154 @@
+package com.example.androidprojectcollection;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.text.DecimalFormat;
+import java.util.Stack;
+
+public class Calculator extends MainActivity3Calc {
+    
+        public double evaluateExpression (String expression){
+
+            String[] tokens = expression.split("(?=[+\\-x/])|(?<=[+\\-x/])");
+
+
+            double result = Double.parseDouble(tokens[0]);
+            char operator = ' ';
+
+
+            for (int i = 1; i < tokens.length; i++) {
+
+                if (tokens[i].matches("[+\\-x/]")) {
+                    operator = tokens[i].charAt(0);
+                } else {
+
+                    double operand = Double.parseDouble(tokens[i]);
+                    switch (operator) {
+                        case '+':
+                            result += operand;
+                            break;
+                        case '-':
+                            result -= operand;
+                            break;
+                        case 'x':
+                            result *= operand;
+                            break;
+                        case '/':
+
+                            if (operand == 0) {
+                                throw new ArithmeticException("Division by zero");
+                            }
+                            result /= operand;
+                            break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public String formatResult ( double result){
+            // Check if the result has decimal places
+            if (result == (long) result) {
+                // If the result is an integer, remove the decimal part
+                return String.format("%d", (long) result);
+            } else {
+
+                DecimalFormat decimalFormat = new DecimalFormat("#.##########");
+                return decimalFormat.format(result);
+            }
+        }
+
+
+
+        public void updateResult (MainActivity3Calc m) {
+            String expression = m.display.getText().toString();
+            if (expression.contains("+") || expression.contains("-") || expression.contains("x") || expression.contains("/")) {
+                try {
+                    double result = evaluateExpression(expression);
+                    String formattedResult = formatResult(result);
+                    m.eqView.setText(formattedResult);
+                } catch (Exception e) {
+                    m.eqView.setText("");
+                }
+            } else {
+                m.eqView.setText("");
+            }
+        }
+
+    public Double ActualResult (String expression){
+        expression = expression.replaceAll("\\s+", "");
+
+
+        String[] tokens = expression.split("(?<=[-+x/()])|(?=[-+x/()])");
+
+
+        Stack<Double> numbers = new Stack<>();
+        Stack<Character> operators = new Stack<>();
+
+
+        for (String token : tokens) {
+
+            if (token.matches("[0-9]+")) {
+                numbers.push(Double.parseDouble(token));
+            } else if (token.equals("(")) {
+                operators.push(token.charAt(0));
+            } else if (token.equals(")")) {
+
+                while (!operators.isEmpty() && operators.peek() != '(') {
+                    evaluateOperator(numbers, operators);
+                }
+                operators.pop(); // Remove the '('
+            } else {
+                while (!operators.isEmpty() && Precedence(token.charAt(0), operators.peek())) {
+                    evaluateOperator(numbers, operators);
+                }
+                operators.push(token.charAt(0));
+            }
+        }
+
+        while (!operators.isEmpty()) {
+            evaluateOperator(numbers, operators);
+        }
+        return numbers.pop();
+
+    }
+
+    public void evaluateOperator (Stack < Double > numbers, Stack < Character > operators){
+        char operator = operators.pop();
+        double num2 = numbers.pop();
+        double num1 = numbers.pop();
+        double result;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case 'x':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    throw new ArithmeticException("Division by zero");
+                }
+                result = num1 / num2;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid operator");
+        }
+        numbers.push(result);
+    }
+
+    public boolean Precedence(char op1, char op2) {
+        if (op2 == '(' || op2 == ')') {
+            return false;
+        }
+        return (op1 != 'x' && op1 != '/') || (op2 != '+' && op2 != '-');
+    }
+
+
+    }
+
--- app/src/main/java/com/example/androidprojectcollection/MainActivity3Calc.java
+++ app/src/main/java/com/example/androidprojectcollection/MainActivity3Calc.java
@@ -1,7 +1,6 @@
 package com.example.androidprojectcollection;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,69 +8,274 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.DecimalFormat;
+import java.util.Stack;
+
 public class MainActivity3Calc extends AppCompatActivity {
 
-    @SuppressLint("MissingInflatedId")
-    @Override
+    Button btn1,
+            btn2,
+            btn3,
+            btn4,
+            btn5,
+            btn6,
+            btn7,
+            btn8,
+            btn9,
+            btn0,
+            AC,
+            btnEq,
+            btnAd,
+            btnSub,
+            btnDiv,
+            btnMult,
+            btnPoi,
+            btnPerc,
+            btnNeg;
+    TextView display,
+            eqView;
+    Boolean Nums = false;
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        Calculator c = new Calculator();
+        btn0 = (Button) findViewById(R.id.btnnum0);
+        btn1 = (Button) findViewById(R.id.btnnum1);
+        btn2 = (Button) findViewById(R.id.btnnum2);
+        btn3 = (Button) findViewById(R.id.btnnum3);
+        btn4 = (Button) findViewById(R.id.btnnum4);
+        btn5 = (Button) findViewById(R.id.btnnum5);
+        btn6 = (Button) findViewById(R.id.btnnum6);
+        btn7 = (Button) findViewById(R.id.btnnum7);
+        btn8 = (Button) findViewById(R.id.btnnum8);
+        btn9 = (Button) findViewById(R.id.btnnum9);
 
-        Button btn1, btn2, btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0;
-        Button AC, btnEq, btnAd, btnSub, btnDiv,btnMult, btnPoi, btnPerc, btnNeg;
-        TextView display;
-
-        display = (TextView) findViewById(R.id.textView);
+        btnPoi = (Button) findViewById(R.id.button86);
+        AC = (Button) findViewById(R.id.button93);
         btnAd = (Button) findViewById(R.id.button83);
-        btnAd.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-
-
-            }
-        });
-
         btnSub = (Button) findViewById(R.id.button80);
-        btnSub.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-
-
-            }
-        });
-
-
         btnMult = (Button) findViewById(R.id.button92);
-        btnMult.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-
-
-            }
-        });
-
-
         btnDiv = (Button) findViewById(R.id.button77);
-        btnDiv.setOnClickListener(new View.OnClickListener() {
+        btnEq = (Button) findViewById(R.id.button87);
 
+        eqView = (TextView) findViewById(R.id.equationview);
+        display= (TextView) findViewById(R.id.textView);
+
+        MainActivity3Calc m = this;
+
+        /*Buttons */
+        btn0.setOnClickListener(new View.OnClickListener()   {
             @Override
             public void onClick(View view) {
-
-
+                if (!display.getText().toString().equals("0")) {
+                    display.append("0");
+                }
+                Nums = true;
+                c.updateResult(m);
             }
         });
 
-        btn1 = (Button) findViewById(R.id.button89);
         btn1.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-                display.setText("1");
+                eqView.append("1");
+
+                Nums = true;
+                c.updateResult(m);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("2");
+
+                Nums = true;
+                c.updateResult(m);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("3");
+
+                Nums = true;
+                c.updateResult(m);
             }
         });
 
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("4");
+
+                Nums = true;
+                c.updateResult(m);
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("5");
+
+                Nums = true;
+                c.updateResult(m);
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("6");
+
+                Nums = true;
+                c.updateResult(m);
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("7");
+
+                Nums = true;
+                c.updateResult(m);
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("8");
+
+                Nums = true;
+                c.updateResult(m);
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("9");
+
+                Nums = true;
+                c.updateResult(m);
+            }
+        });
+
+        /* Operations*/
+
+        AC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.setText("");
+            }
+        });
+
+        btnAd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StringBuilder tmp = new StringBuilder();
+                tmp.append(eqView.getText().toString());
+                int ctr = tmp.length() - 1;
+                if (ctr >= 0 && (tmp.charAt(ctr) == '-' || tmp.charAt(ctr) == 'x' || tmp.charAt(ctr) == '/')) {
+                    eqView.setText(tmp.substring(0, ctr) + "+");
+                } else if (!Nums) {
+                    eqView.append("+");
+
+                    Nums = true;
+                }
+
+
+            }
+        });
+
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StringBuilder tmp = new StringBuilder();
+                tmp.append(eqView.getText().toString());
+                int ctr = tmp.length() - 1;
+                if (tmp.charAt(ctr) == '+' || tmp.charAt(ctr) == 'x' || tmp.charAt(ctr) == '/') {
+                    eqView.setText(tmp.substring(0, ctr) + "-");
+                } else if (Nums) {
+                    eqView.append("-");
+
+                    Nums = false;
+                }
+
+
+            }
+        });
+
+        btnMult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StringBuilder tmp = new StringBuilder();
+                tmp.append(eqView.getText().toString());
+                if (tmp.charAt(tmp.length() - 1) == '+' || tmp.charAt(tmp.length() - 1) == '-' || tmp.charAt(tmp.length() - 1) == '/') {
+                    eqView.setText(tmp.substring(0, (tmp.length() - 1)) + "x");
+                } else if (Nums) {
+                    eqView.append("x");
+
+                    Nums = false;
+                }
+
+
+            }
+        });
+
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StringBuilder tmp = new StringBuilder();
+                tmp.append(eqView.getText().toString());
+                if (tmp.charAt(tmp.length() - 1) == '+' || tmp.charAt(tmp.length() - 1) == '-' || tmp.charAt(tmp.length() - 1) == 'x') {
+                    eqView.setText(tmp.substring(0, (tmp.length() - 1)) + "/");
+                } else if (Nums) {
+                    eqView.append("/");
+
+                    Nums = false;
+                }
+
+
+            }
+        });
+
+
+        btnPoi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StringBuilder tmp = new StringBuilder(eqView.getText().toString());
+                if (Nums && tmp.charAt(tmp.length() - 1) != '.') {
+                    eqView.append(".");
+                } else if (!Nums) {
+                    eqView.append("0.");
+                    Nums = true;
+                }
+
+            }
+        });
+
+
+        btnEq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String expression = eqView.getText().toString();
+                try {
+                    double result = c.ActualResult(expression);
+                    String formattedResult = c.formatResult(result);
+                    System.out.println(formattedResult);
+                    display.setText("");
+                    display.setText(formattedResult);
+
+                } catch (Exception e) {
+                    display.setText("Error");
+                }
+            }
+        });
     }
 }
+
+
+
+
+
+
